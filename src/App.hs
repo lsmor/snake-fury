71f20541e1605c46ddd7fac89a81d34f65f5fdef
Luis Morillo <luis_cnnvd@hotmail.com>
2022-07-31 13:31:28 +0200

    create DSL based on class constraints

--- src/App.hs
+++ src/App.hs
@@ -1,20 +1,30 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module App where
-import GameState (GameState, move, HasGameState (getGameState, setGameState))
-import RenderState (RenderState (score), BoardInfo, render, HasRenderState (getRenderState, setRenderState))
+import GameState (GameState, move, HasGameState (getGameState, setGameState), Event)
+import RenderState (RenderState (score), BoardInfo, HasRenderState (getRenderState, setRenderState), RenderMessage, updateMessages, HasBoardInfo (getBoardInfo), buildBoard)
+import qualified RenderState
 import Control.Monad.Reader (MonadReader (ask), asks, ReaderT (runReaderT))
 import Control.Monad.State (MonadState (get), gets, StateT (runStateT), evalStateT)
 import Control.Monad.IO.Class (MonadIO (liftIO))
-import EventQueue (EventQueue, readEvent, setSpeed)
-import Control.Concurrent (threadDelay)
+import EventQueue (EventQueue (initialSpeed, currentSpeed), readEvent, setSpeed, calculateSpeed, HasEventQueue (getEventQueue))
+import Control.Concurrent (threadDelay, readMVar, putMVar)
 import Control.Monad (forever)
+import qualified Data.ByteString.Builder as B
+import System.IO (stdout)
 
 
 data AppState = AppState GameState RenderState
-newtype App m a = App {runApp :: ReaderT BoardInfo (StateT AppState m) a}
-  deriving (Functor , Applicative, Monad, MonadState AppState, MonadReader BoardInfo, MonadIO)
+data Env = Env BoardInfo EventQueue
+newtype App m a = App {runApp :: ReaderT Env (StateT AppState m) a}
+  deriving (Functor , Applicative, Monad, MonadState AppState, MonadReader Env, MonadIO)
+
+{-
+Instances for the State of the application.
+-}
 
 instance HasGameState AppState where
   getGameState (AppState g _ )   = g
@@ -24,16 +34,72 @@ instance HasRenderState AppState where
   getRenderState (AppState _ r )   = r
   setRenderState (AppState g _ ) r = AppState g r
 
+{-
+The instances for Environment.
+-}
 
-gameStep :: (MonadReader BoardInfo m, MonadState state m, HasGameState state, HasRenderState state, MonadIO m) => EventQueue -> m ()
-gameStep queue = liftIO (readEvent queue) >>= move >>= render
+instance HasBoardInfo Env where
+  getBoardInfo (Env b _ ) = b
 
-gameloop :: (MonadReader BoardInfo m, MonadState state m, HasGameState state, HasRenderState state, MonadIO m) => EventQueue -> m ()
-gameloop queue = forever $ do
-  s <- gets (score . getRenderState)
-  new_speed <- liftIO $ setSpeed s queue
-  liftIO $ threadDelay new_speed
-  gameStep queue
+instance HasEventQueue Env where
+  getEventQueue (Env _ q) = q
 
-run :: BoardInfo -> AppState -> EventQueue -> IO ()
-run binf app queue = runApp (gameloop queue) `runReaderT` binf `evalStateT` app
+
+{-
+Our small DSL for working with all components of the software
+-}
+
+class Monad m => MonadQueue m where
+  pullEvent :: m Event          -- ^Pull an Event from the queue
+
+class Monad m => MonadSnake m where
+  updateGameState :: Event -> m [RenderMessage]
+  updateRenderState :: [RenderMessage] -> m ()
+
+class Monad m => MonadRender m where
+  render :: m ()
+
+
+{-
+The instances for App monad.
+-}
+
+instance (MonadIO m) => MonadQueue (App m) where
+  pullEvent = do
+    q <- asks getEventQueue
+    liftIO $ readEvent q
+
+instance Monad m => MonadSnake (App m) where
+  updateGameState = move
+  updateRenderState = updateMessages
+
+instance (MonadIO m) => MonadRender (App m) where
+  render = do
+    binf <- asks getBoardInfo
+    rstate <- gets getRenderState
+    liftIO $ putStr "\ESC[2J" --This cleans the console screen
+    liftIO $ B.hPutBuilder stdout (buildBoard binf rstate)
+
+
+{-
+The main logic of the game
+-}
+
+setSpeedOnScore :: (MonadReader env m, HasEventQueue env, MonadState state m, HasRenderState state, MonadIO m) => m Int
+setSpeedOnScore = do
+  current_score <- gets (score . getRenderState) -- Read the current reference to speed
+  queue         <- asks getEventQueue
+  liftIO $ setSpeed current_score queue
+
+
+gameStep :: (MonadQueue m, MonadSnake m, MonadRender m) => m ()
+gameStep = pullEvent >>= updateGameState >>= updateRenderState >> render
+
+gameloop :: (MonadQueue m, MonadSnake m, MonadRender m, MonadState state m, HasRenderState state, MonadReader env m, HasEventQueue env, MonadIO m) => m ()
+gameloop = forever $ do
+  w <- setSpeedOnScore 
+  liftIO $ threadDelay w
+  gameStep
+
+run :: Env -> AppState -> IO ()
+run env app = runApp gameloop `runReaderT` env `evalStateT` app
