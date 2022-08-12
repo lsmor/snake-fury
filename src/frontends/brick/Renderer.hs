1ccd46953d543c005ba1aa91e456237ace9a6f77
Luis Morillo <luis_cnnvd@hotmail.com>
2022-08-12 09:55:01 +0200

    try brick

--- src/frontends/brick/Renderer.hs
+++ src/frontends/brick/Renderer.hs
@@ -4,3 +4,27 @@
 {-# LANGUAGE UndecidableInstances #-}
 
 module Renderer where
+import Brick (Widget, CursorLocation, BrickEvent, EventM, AttrMap)
+import Brick.Main (App)
+import GameState (GameState(..))
+import RenderState (RenderState(..), BoardInfo (BoardInfo))
+import EventQueue (EventQueue(..))
+
+
+data AppState = AppState GameState RenderState
+data Env = Env BoardInfo EventQueue
+
+myAppDraw :: AppState -> [Widget n]
+myAppDraw = undefined
+
+myAppChooseCursor :: AppState -> [CursorLocation n] -> Maybe (CursorLocation n)
+myAppChooseCursor = undefined
+
+myAppHandleEvent :: BrickEvent n e -> EventM n s ()
+myAppHandleEvent = undefined
+
+myAppStartEvent :: EventM n AppState ()
+myAppStartEvent = undefined
+
+myAppAttrMap :: AppState -> AttrMap
+myAppAttrMap = undefined
